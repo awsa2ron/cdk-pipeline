d85fb26ba42f66b6c64edb2f6590570e132d22eb
Aaron Tsui <kaixu@amazon.com>
2022-03-03 09:23:15 +0800

    deploy a pipeline manually once.

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,16 +1,25 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      const pipeline = new CodePipeline(this, 'Pipeline', {
+        pipelineName: 'cdkPipeline',
+        synth: new ShellStep('Synth', {
+          input: CodePipelineSource.connection('awsa2ron/cdk-pipeline', 'main',{
+						connectionArn:
+							"arn:aws:codestar-connections:us-west-2:839607536172:connection/0ff04336-df6f-4ecf-a9f4-951f30cffe9a"
+					}),
+                              installCommands: [
+                        'npm i -g npm && npm ci'
+                    ],
+          commands: ['npm run build',
+                     'npx cdk synth']
+        })
+      });
   }
 }
